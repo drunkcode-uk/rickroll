bf06613b34fb83b30ff832582f2837c57fa71524
Ray Blair <rblair@codeplayground.io>
2020-11-20 15:00:47 +0000

    Only Rick Roll requests that are GET and that accepts html
    Signed-off-by: Ray Blair <rblair@codeplayground.io>

--- src/Http/Middleware/RickRollMiddleware.php
+++ src/Http/Middleware/RickRollMiddleware.php
@@ -15,6 +15,16 @@ class RickRollMiddleware
      */
     public function handle($request, Closure $next)
     {
+        // If our Request doesn't accept html, exit early with next request
+        if (!$request->acceptsHtml()) {
+            return $next($request);
+        }
+
+        // If out Request isn't a get requets, exit early with next request
+        if ($request->method() != 'GET') {
+            return $next($request);
+        }
+
         // Randomly roll if the user is gonna get RickRoll'd!
         $rick_roll = (rand(0, config('rickroll.odds')) == 0) ? true : false;
 
